7b2cf25bd2de215f9e300e7c56fc72f3557630e4
Nelida27 <48954530+Nelida27@users.noreply.github.com>
2021-05-27 01:23:43 +0200

    breadcrumb

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -7,13 +7,15 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 @NgModule({
   declarations: [
     PageNotFoundComponent,
     NavbarComponent,
     PageNotFoundComponent,
     SidebarComponent,
-    HomeComponent
+    HomeComponent,
+    BreadcrumbComponent
   ],
   imports: [
     CommonModule,
